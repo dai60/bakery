5fdbb4e0968382f45ec28ec358529867db844f4a
dai60 <190627676+dai60@users.noreply.github.com>
2024-12-09 13:32:52 +0200

    new scripts

--- scripts.js
+++ scripts.js
@@ -5,3 +5,45 @@ Papildykite projektą:
 - Pridėti daugiau įvesties laukelių ir/ar skaičiavimų iš duotos informacijos.
 - Pridėti informacinių tekstų, kurie paaiškintų ką kuris laukelis ar skaičiavimas reiškia ir pan.
 */
+
+document.getElementById("calculate").addEventListener("click", function () {
+    const employeeCount = document.getElementById("employee-count").valueAsNumber;
+    const hoursPerDay = document.getElementById("hours-per-day").valueAsNumber;
+    const loavesPerEmployee = document.getElementById("loaves-per-employee").valueAsNumber;
+    const orderCount = document.getElementById("order-count").valueAsNumber;
+
+    const totalLoaves = Math.floor(employeeCount * loavesPerEmployee * hoursPerDay);
+    const targetReached = totalLoaves >= orderCount;
+    const missing = orderCount - totalLoaves;
+
+    const results = document.getElementById("results");
+    results.innerHTML = `<p><strong>Kepykla per dieną spės pagaminti:</strong> ${totalLoaves} kepalų</p>`;
+    results.innerHTML += `<p><strong>Reikia pagaminti:</strong> ${orderCount} kepalų</p>`;
+    results.innerHTML += `<p><strong>Ar spės pagaminti?</strong> ${targetReached ? "Taip." : `Ne. Trūks ${missing} kepalų.`}</p>`;
+});
+
+document.getElementById("reset").addEventListener("click", function () {
+    document.getElementById("employee-count").valueAsNumber = 0;
+    document.getElementById("hours-per-day").valueAsNumber = 8;
+    document.getElementById("loaves-per-employee").valueAsNumber = 0;
+    document.getElementById("order-count").valueAsNumber = 0;
+
+    document.getElementById("results").innerHTML = "<p>Kol kas nieko nėra.</p>";
+});
+
+document.querySelectorAll("input").forEach(function (element) {
+    element.addEventListener("input", function (event) {
+        const value = event.target.valueAsNumber;
+        const min = parseInt(event.target.min);
+        const max = parseInt(event.target.max);
+
+        if ((!isNaN(min) && value < min) || (!isNaN(max) && value > max)) {
+            event.target.classList.add("error");
+            event.target.nextElementSibling.classList.add("show");
+        }
+        else {
+            event.target.classList.remove("error");
+            event.target.nextElementSibling.classList.remove("show");
+        }
+    });
+});
